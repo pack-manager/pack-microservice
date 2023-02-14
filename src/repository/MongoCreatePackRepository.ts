4988b6c70df0e966bfeb38634473c234846887f5
Matheus Evangelista <matheus.evangelista@g.globo>
2023-02-13 21:11:08 -0300

    feat: add verification step to not create a package if it already exists

--- src/repository/MongoCreatePackRepository.ts
+++ src/repository/MongoCreatePackRepository.ts
@@ -7,19 +7,27 @@ import { MongoOmit } from "./protocol/MongoOmit";
 
 export class MongoCreatePackRepository implements ICreatePackRepository {
     async createPack(params: CreatePackParams): Promise<IPack> {
+        const hasPack = await MongoClient.db
+            .collection<MongoOmit>("packs")
+            .findOne({ code: params.code })
+
+        if (hasPack) {
+            throw AppErrorFactory.create(HttpStatusCode.CONFLICT, "Pack already exists")
+        }
+
         const { insertedId } = await MongoClient.db
             .collection("packs")
             .insertOne(params)
 
-        const pack = await MongoClient.db
+        const createdPack = await MongoClient.db
             .collection<MongoOmit>("packs")
             .findOne({ _id: insertedId })
 
-        if (!pack) {
+        if (!createdPack) {
             throw AppErrorFactory.create(HttpStatusCode.BAD_REQUEST, "Pack not created")
         }
 
-        const { _id, ...rest } = pack
+        const { _id, ...rest } = createdPack
         return { id: _id.toHexString(), ...rest }
     }
 } 
