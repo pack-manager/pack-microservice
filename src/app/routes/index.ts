7a5e392da8720db9f112eb5e4cbdb039bbb3c412
Matheus Evangelista <matheus.evangelista@g.globo>
2023-04-15 06:50:58 -0300

    Add cors

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -9,27 +9,27 @@ const routes = Router()
 
 routes.post("/packs", async (req, res) => {
     const { body, statusCode } = await createPackController.handle({ body: req.body })
-    res.status(statusCode).send(body)
+    res.status(statusCode).json(body)
 })
 
 routes.get("/packs/users/:userId", async (req, res) => {
     const { body, statusCode } = await getPacksFromUserController.handle({ params: req.params })
-    res.status(statusCode).send(body)
+    res.status(statusCode).json(body)
 })
 
 routes.get("/packs/:id", async (req, res) => {
     const { body, statusCode } = await getPackController.handle({ params: req.params })
-    res.status(statusCode).send(body)
+    res.status(statusCode).json(body)
 })
 
 routes.patch("/packs/:id", async (req, res) => {
     const { body, statusCode } = await updatePackController.handle({ params: req.params, body: req.body })
-    res.status(statusCode).send(body)
+    res.status(statusCode).json(body)
 })
 
 routes.delete("/packs/:id", async (req, res) => {
     const { body, statusCode } = await deletePackController.handle({ params: req.params })
-    res.status(statusCode).send(body)
+    res.status(statusCode).json(body)
 })
 
 export default routes
